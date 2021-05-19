765f8ca9b097bdceb70b26af7b4e8b5f324537f5
e.makukhin <maku@mail.ru>
2021-05-19 13:31:53 +0300

    Урок первый. Запускаем Django REST framework.

---
+++ djrest/users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=128, unique=True)
