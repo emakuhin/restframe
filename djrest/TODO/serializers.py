ff87aaad8f29bd464fa2c07e114c36cb4d18f77e
e.makukhin <maku@mail.ru>
2021-05-28 16:13:25 +0300

    Домашнее задание. Урок 4. Не получилось переопределить метод delete.

--- djrest/TODO/serializers.py
+++ djrest/TODO/serializers.py
@@ -7,13 +7,13 @@ from TODO.models import Project, Todo
 
 
 
-class Todoserializer(HyperlinkedModelSerializer):
+class Todoserializer(ModelSerializer):
 #    project = StringRelatedField()
     class Meta:
         model = Todo
         fields = '__all__'
 
-class Projectserializer(HyperlinkedModelSerializer):
+class Projectserializer(ModelSerializer):
     todos = Todoserializer(many=True, read_only=True)
     class Meta:
         model = Project
--- djrest/TODO/views.py
+++ djrest/TODO/views.py
@@ -5,6 +5,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from TODO.filters import ProjectFilter, TodoFilter
 from rest_framework.decorators import action
 from rest_framework.response import Response
+import pprint
 
 class ProjectLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 10
@@ -26,9 +27,14 @@ class TodoModelViewSet(ModelViewSet):
     pagination_class = TodoLimitOffsetPagination
     filterset_class = TodoFilter
     def destroy(self, request, *args, **kwargs):  # Я был Близок, протыкался два дня, так ничего и не получилось.
-        k = Todo.objects.get(pk=kwargs['pk'])
-        print(k)
-        return Response(kwargs)
+        todo = Todo.objects.filter(id=kwargs['pk'])
+        serializer = Todoserializer(todo, many=True)
+        serializer.data[0]['active'] = False
+        pprint.pprint(serializer.data[0]['active'])
+
+ #       serializer.update(self, serializer.data[0], f)
+        return Response(serializer.data)
+
 
 
 
