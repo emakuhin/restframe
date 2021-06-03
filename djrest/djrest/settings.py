da620459d37d4fff27d2f29982db9fed70b88fab
e.makukhin <maku@mail.ru>
2021-06-03 17:35:26 +0300

    Урок 7. Авторизация на стороне клиента.

--- djrest/djrest/settings.py
+++ djrest/djrest/settings.py
@@ -28,9 +28,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-   "http://localhost:3000",
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000',
 ]
-AUTH_USER_MODEL = 'users.User'
+
 
 # Application definition
 
@@ -48,6 +50,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_filters',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
--- djrest/frontend/src/App.js
+++ djrest/frontend/src/App.js
@@ -6,6 +6,8 @@ import TodoList from './components/todos.js'
 import ProjectList from './components/projects.js'
 import axios from 'axios'
 import {HashRouter, Route, Link, Switch, Redirect} from 'react-router-dom'
+import LoginForm from './components/Auth.js'
+import Cookies from 'universal-cookie';
 
 
 const NotFound404 = ({ location }) => {
@@ -17,55 +19,83 @@ const NotFound404 = ({ location }) => {
 }
 
 class App extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
             'todos': [],
             'projects': [],
-            'users': []
+            'users': [],
+            'token': ''
         }
+    }
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+    is_authenticated() {
+        return this.state.token != ''
+    }
+    logout() {
+        this.set_token('')
+    }
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+    get_headers() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_authenticated())
+        {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+    load_data() {
+        const headers = this.get_headers()
+        axios.get('http://127.0.0.1:8000/api/todos/', {headers})
+            .then(response => {
+                this.setState({todos: response.data})
+            }).catch(error => {console.log(error)
+            this.setState({todos: []})
+            })
+        axios.get('http://127.0.0.1:8000/api/projects/', {headers})
+            .then(response => {
+                this.setState({projects: response.data})
+            }).catch(error => {console.log(error)
+            this.setState({projects: []})
+            })
+        axios.get('http://127.0.0.1:8000/api/users/', {headers})
+            .then(response => {
+                this.setState({users: response.data})
+            }).catch(error => {console.log(error)
+            this.setState({users: []})
+            })
     }
 
     componentDidMount() {
-       axios
-       .get('http://127.0.0.1:8000/api/todos/')
-       .then(response => {
-           const todos = response.data
-               this.setState(
-               {
-                   'todos': todos
-               }
-           )
-       }).catch(error => console.log(error))
-       axios
-       .get('http://127.0.0.1:8000/api/users/')
-       .then(response => {
-           const users = response.data
-               this.setState(
-               {
-                   'users': users
-               }
-           )
-       }).catch(error => console.log(error))
-       axios
-       .get('http://127.0.0.1:8000/api/projects/')
-       .then(response => {
-           const projects = response.data
-               this.setState(
-               {
-                   'projects': projects
-               }
-           )
-       }).catch(error => console.log(error))
+        this.get_token_from_storage()
        }
+    get_token(username, password) {
+       axios
+       .post('http://127.0.0.1:8000/api-token-auth/',
+            {"username": username, "password": password}
+       )
+        .then(response => {
+            this.set_token(response.data['token'])
+        }).catch(error => alert('Неверный логин или пароль'))
+     }
 
     render() {
         return (
             <div>
             <HashRouter>
-                <div class='header'>
-                                    <ul>
+            <nav  class='navbar navbar-inverse' role='navigation'>
+                <div class='container'>
+                        <ul class='nav navbar-nav'>
                             <li>
                                 <Link to='/users'>Users</Link>
                             </li>
@@ -74,14 +104,19 @@ class App extends React.Component {
                             </li>
                             <li>
                                 <Link to='/projects'>Projects</Link>
-                            </li>
+                             </li>
+                             <li>
+                                {this.is_authenticated() ? <button onClick={()=>this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
+                             </li>
                         </ul>
                 </div>
+                </nav>
                 <div>
                     <Switch>
                      <Route exact path='/users' component = {() => <UserList users={this.state.users} />} />
                      <Route exact path='/todos' component = {() => <TodoList todos={this.state.todos} />} />
                      <Route exact path='/projects' component = {() => <ProjectList projects={this.state.projects} />} />
+                     <Route exact path='/login' component = {() => <LoginForm get_token={(username, password) => this.get_token(username, password)}/>} />
                      <Redirect from='/' to='users' />
                      <Route component={NotFound404} />
             </Switch>
@@ -92,7 +127,7 @@ class App extends React.Component {
                     Footer
                 </div>
                 </HashRouter>
-            </div>
+                </div>
         )
     }
 }
